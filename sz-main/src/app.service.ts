eed081add3c3690061e3c442b397fd65e64fd782
Igor Zalutski <izalutski@gmail.com>
2021-06-02 17:55:58 +0100

    Make sz-main talk to pdf-svc via TCP

--- sz-main/src/app.service.ts
+++ sz-main/src/app.service.ts
@@ -1,8 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Inject } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
 
 @Injectable()
 export class AppService {
+  constructor(
+    @Inject('PDF_SERVICE') private client: ClientProxy,
+  ) {}
   getHello(): string {
+    const pattern = { cmd: 'hello' };
+    this.client.send(pattern, {}).subscribe({
+      next: (data) => console.log(data),
+      error: (err) => console.error('Error:', err)
+    });
     return 'Hello World!';
   }
 }
